4529c7de4f3b62b4ebe4a953321c07c932bcaedd
anshulgaur18 <102699994+anshulgaur18@users.noreply.github.com>
2022-11-25 18:03:16 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
    <>
     <Routes>
-      <Route path='/' element={<Home/>}/>
+      <Route path='/Portfolio_Anshul_Gaur' element={<Home/>}/>
       <Route path='/about' element={<About/>}/>
       <Route path='/contact' element={<Contact/>}/>
       <Route path='/project' element={<Project/>}/>
